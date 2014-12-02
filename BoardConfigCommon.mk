e60630444b0f668ca2a62883c431a3e7f06a9880
Y300-0100 <bosko.brajovich@gmail.com>
2014-12-02 23:19:22 +0100

    initial commit

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,135 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+# inherit from the proprietary version
+-include vendor/lge/msm7x27a-common/BoardConfigVendor.mk
+
+TARGET_SPECIFIC_HEADER_PATH := device/lge/msm7x27a-common/include
+
+BOARD_VENDOR := lge
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := msm7x27a
+
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a5
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+
+TARGET_BOARD_PLATFORM := msm7x27a
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+BOARD_USES_ADRENO_200 := true
+
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_ARCH_LOWMEM := true
+
+TARGET_KERNEL_SOURCE := kernel/lg/p715/kernel
+BOARD_KERNEL_BASE := 0x00200000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x1200000
+BOARD_KERNEL_PAGESIZE := 4096
+#Spare size is (BOARD_KERNEL_PAGESIZE>>9)*16
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 487136952
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2920577761
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_VOLD_MAX_PARTITIONS := 23
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# cflags
+BOARD_USES_QCOM_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_BSP -DQCOM_HARDWARE
+TARGET_USES_ION := true
+TARGET_USES_QCOM_BSP := true
+
+TARGET_GLOBAL_CFLAGS += -mfloat-abi=softfp -mfpu=neon-vfpv4 -mtune=cortex-a5
+TARGET_GLOBAL_CPPFLAGS += -mfloat-abi=softfp -mfpu=neon-vfpv4 -mtune=cortex-a5
+
+# QCOM  display stuffs
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+BOARD_EGL_CFG := device/lge/msm7x27a-common/egl.cfg
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+BOARD_USE_MHEAP_SCREENSHOT := true
+TARGET_DOESNT_USE_FENCE_SYNC := true
+TARGET_QCOM_DISPLAY_VARIANT := legacy
+USE_OPENGL_RENDERER := true
+
+# Media
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+TARGET_QCOM_MEDIA_VARIANT := legacy
+
+# bluetooth
+BOARD_HAVE_BLUETOOTH := true
+
+# audio 
+TARGET_QCOM_AUDIO_VARIANT := caf
+TARGET_PROVIDES_LIBAUDIO := true
+BOARD_QCOM_VOIP_ENABLED := true
+BOARD_USES_LEGACY_ALSA_AUDIO := true
+TARGET_HAS_QACT := true
+
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm7x27a
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+TARGET_NO_INITLOGO := true
+
+TARGET_PROVIDES_LIBLIGHT := true
+
+BOARD_SEPOLICY_DIRS := \
+       device/lge/msm7x27a-common/sepolicy
+
+BOARD_SEPOLICY_UNION := \
+       device.te \
+       app.te \
+       file_contexts
+
+BOARD_HAS_QCOM_WLAN              := true
+BOARD_HAS_QCOM_WLAN_SDK          := true
+BOARD_WLAN_DEVICE                := qcwcn
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+TARGET_CUSTOM_WIFI := ../../device/lge/p715/libhardware_legacy/wifi/wifi.c
+WIFI_EXT_MODULE_PATH             := "/system/lib/modules/librasdioif.ko"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
+WIFI_EXT_MODULE_NAME             := "librasdioif"
+WIFI_DRIVER_MODULE_NAME          := "wlan"
+WIFI_DRIVER_FW_PATH_STA          := "sta"
+WIFI_DRIVER_FW_PATH_AP           := "ap"
+WIFI_DRIVER_FW_PATH_P2P          := "p2p"
+WIFI_DRIVER_FW_PATH_PARAM        := "/data/misc/wifi/fwpath"
